735ffea8384c3ed786367f91eaaa2669ee1d4f12
Pablo Hernandez <pdhernandez05@gmail.com>
2021-09-18 08:31:16 -0500

    First Commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
-$(document).ready(function(){
+$(function(){
     $('.jumbotron').height($(window).height());
     
-   })
+    $(".carousel").carousel( { interval: 5000, pause: "false" } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("span").removeClass("fa-pause");
+            $("#carouselButton").children("span").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("span").removeClass("fa-play");
+            $("#carouselButton").children("span").addClass("fa-pause");
+        }
+    });
+});
